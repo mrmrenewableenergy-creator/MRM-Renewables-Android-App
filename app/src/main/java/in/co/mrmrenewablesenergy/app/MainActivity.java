47f8a4f835fba0408b04979f1361972241586685
mrmrenewableenergy-creator <mrmrenewableenergy@gmail.com>
2026-09-13 13:18:26 +0530

    Improve full-page CRM fit and zoom in APK

--- app/src/main/java/in/co/mrmrenewablesenergy/app/MainActivity.java
+++ app/src/main/java/in/co/mrmrenewablesenergy/app/MainActivity.java
@@ -10,23 +10,38 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     private WebView webView;
+
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         webView = new WebView(this);
         setContentView(webView);
+
         WebSettings s = webView.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
         s.setAllowFileAccess(true);
         s.setLoadWithOverviewMode(true);
         s.setUseWideViewPort(true);
+        s.setSupportZoom(true);
+        s.setBuiltInZoomControls(true);
+        s.setDisplayZoomControls(false);
+        s.setTextZoom(100);
+
         CookieManager.getInstance().setAcceptCookie(true);
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
+        webView.setInitialScale(70);
         webView.loadUrl("https://mrmrenewablesenergy.co.in");
     }
+
     @Override public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
